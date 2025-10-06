25396172f28f6e6056aa5620fb656c6bb0503bac
Baard Johansen <baard.johansen@sesam.io>
2025-10-06 19:11:42 +0200

    add unit tests

---
+++ src/dtl.rs
@@ -0,0 +1,164 @@
+use serde_json::Value;
+pub struct Target {
+    target: Value,
+    filtered: bool,
+    created_targets: Vec<Value>,
+}
+
+impl Target {
+    pub fn new() -> Self {
+        Target {
+            target: Value::Object(serde_json::Map::new()),
+            filtered: false,
+            created_targets: Vec::new(),
+        }
+    }
+
+    pub fn add(&mut self, property_name: &'static str, value: Value) {
+        match self.target {
+            Value::Object(ref mut map) => {
+                map.insert(property_name.into(), value);
+            }
+            _ => {}
+        }
+    }
+
+    pub fn output(&self) -> Vec<Value> {
+        let mut output = self.created_targets.clone();
+        if !self.filtered {
+            output.push(self.target.clone());
+        }
+        output
+    }
+
+    pub fn filter(&mut self) {
+        self.filtered = true;
+    }
+
+    pub fn create(&mut self, value: Value) {
+        match value {
+            Value::Array(arr) => self.created_targets.extend(arr),
+            v => self.created_targets.push(v),
+        }
+    }
+}
+
+fn string_helper(source: &Value, function: impl Fn(&String) -> String) -> Value {
+    match source {
+        Value::Array(arr) => Value::Array(
+            arr.into_iter()
+                .filter_map(|s| match s {
+                    Value::String(s) => Some(Value::String(function(s))),
+                    _ => None,
+                })
+                .collect(),
+        ),
+        Value::String(s) => Value::String(function(s)),
+        _ => return Value::Array(vec![]),
+    }
+}
+
+pub fn lower(source: &Value) -> Value {
+    string_helper(source, |s| s.to_lowercase())
+}
+
+pub fn upper(source: &Value) -> Value {
+    string_helper(source, |s| s.to_uppercase())
+}
+
+pub fn list_literal(content: &[Value]) -> Value {
+    Value::Array(content.to_vec())
+}
+
+pub fn null_literal() -> Value {
+    Value::Null
+}
+
+pub fn number_literal(n: i32) -> Value {
+    Value::Number(n.into())
+}
+
+pub fn string_literal(s: &str) -> Value {
+    Value::String(s.to_string())
+}
+
+pub fn concat(parts: &Value) -> Value {
+    match parts {
+        Value::String(s) => Value::String(s.clone()),
+        Value::Array(arr) => {
+            let mut s = String::new();
+            for part in arr {
+                if let Value::String(ref ss) = part {
+                    s.push_str(ss);
+                }
+            }
+            Value::String(s)
+        }
+        _ => Value::String(String::new()),
+    }
+}
+
+pub fn apply(function: impl Fn(&Value) -> Vec<Value>, items: &Value) -> Value {
+    match items {
+        Value::Array(arr) => Value::Array(arr.iter().flat_map(|v| function(v)).collect()),
+        _ => Value::Array(vec![]),
+    }
+}
+
+pub fn map(function: impl Fn(&Value) -> Value, items: &Value) -> Value {
+    match items {
+        Value::Array(arr) => Value::Array(arr.iter().map(|item| function(item)).collect()),
+        _ => Value::Null,
+    }
+}
+
+pub fn path<'a>(arg: Value, value: &'a Value) -> &'a Value {
+    fn eval_path<'a>(arg: &[&str], value: &'a Value) -> &'a Value {
+        if arg.is_empty() {
+            return value;
+        }
+        let (first, rest) = arg.split_first().unwrap();
+        if let Value::Object(map) = value {
+            if let Some(v) = map.get(*first) {
+                return eval_path(rest, v);
+            }
+        }
+        &Value::Null
+    }
+
+    match arg {
+        Value::String(s) => eval_path(&[&s], value),
+        Value::Array(arr) => {
+            let paths: Vec<&str> = arr
+                .iter()
+                .filter_map(|v| {
+                    if let Value::String(s) = v {
+                        Some(s.as_str())
+                    } else {
+                        None
+                    }
+                })
+                .collect();
+            eval_path(&paths, value)
+        }
+        _ => &Value::Null,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+    use serde_json::json; // optional, nicer diffs
+
+    #[test]
+    fn test_lower() {
+        assert_eq!(json!(["a", "b"]), lower(&json!(["a", "B", 1, null, []])));
+    }
+
+    #[test]
+    fn test_concat() {
+        assert_eq!(json!("aB"), concat(&json!(["a", "B", 1, null, []])));
+        assert_eq!(json!("a"), concat(&json!("a")));
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 use serde_json::Value;
 
+use crate::dtl::{*};
+
+mod dtl;
+
+// TODO think about extension types, datetime, etc, should we make a separate DtlValue enum?
+
 /*
 
            [
@@ -26,19 +32,20 @@ fn hello_world2(source: &Value) -> Vec<Value> {
     let mut target = Target::new();
     target.add(
         "hello",
-        concat(&[
+        //TODO think about list handling and when to use references
+        concat(&list_literal(&[
             string_literal("wor"),
             number_literal(1),
-            concat(&[
+            concat(&list_literal(&[
                 string_literal("l"),
                 lower(path(
                     list_literal(&[string_literal("x"), string_literal("y")]),
                     source,
                 )),
                 null_literal(),
-            ]),
+            ])),
         ]),
-    );
+    ));
     target.output()
 }
 
@@ -78,7 +85,7 @@ fn create_foo2(source: &Value) -> Vec<Value> {
               ]
             ]
 */
-fn map_upper2(source: &Value) -> Vec<Value> {
+fn map_upper2(_: &Value) -> Vec<Value> {
     let mut target = Target::new();
     target.add(
         "bar",
@@ -92,146 +99,6 @@ fn map_upper2(source: &Value) -> Vec<Value> {
         ),
     );
     target.output()
-}
-
-struct Target {
-    target: Value,
-    filtered: bool,
-    created_targets: Vec<Value>,
-}
-
-impl Target {
-    fn new() -> Self {
-        Target {
-            target: Value::Object(serde_json::Map::new()),
-            filtered: false,
-            created_targets: Vec::new(),
-        }
-    }
-
-    fn add(&mut self, property_name: &'static str, value: Value) {
-        match self.target {
-            Value::Object(ref mut map) => {
-                map.insert(property_name.into(), value);
-            }
-            _ => {}
-        }
-    }
-
-    fn output(&self) -> Vec<Value> {
-        let mut output = self.created_targets.clone();
-        if !self.filtered {
-            output.push(self.target.clone());
-        }
-        output
-    }
-
-    fn filter(&mut self) {
-        self.filtered = true;
-    }
-
-    fn create(&mut self, value: Value) {
-        match value {
-            Value::Array(arr) => self.created_targets.extend(arr),
-            v => self.created_targets.push(v),
-        }
-    }
-}
-
-fn lower(source: &Value) -> Value {
-    match source {
-        Value::Array(arr) => Value::Array(
-            arr.into_iter()
-                .map(|s| match s {
-                    Value::String(s) => Value::String(s.to_lowercase()),
-                    _ => Value::Null,
-                })
-                .collect(),
-        ),
-        Value::String(s) => Value::String(s.to_lowercase()),
-        _ => return Value::Array(vec![]),
-    }
-}
-
-// TODO refactor with above
-fn upper(s: &Value) -> Value {
-    match s {
-        Value::String(s) => Value::String(s.to_uppercase()),
-        _ => Value::Null,
-    }
-}
-
-fn list_literal(content: &[Value]) -> Value {
-    Value::Array(content.to_vec())
-}
-
-fn null_literal() -> Value {
-    Value::Null
-}
-
-fn number_literal(n: i32) -> Value {
-    Value::Number(n.into())
-}
-
-fn string_literal(s: &str) -> Value {
-    Value::String(s.to_string())
-}
-
-fn concat(parts: &[Value]) -> Value {
-    let mut s = String::new();
-    for part in parts {
-        if let Value::String(ref ss) = part {
-            s.push_str(ss);
-        }
-    }
-    Value::String(s)
-}
-
-fn apply(function: impl Fn(&Value) -> Vec<Value>, items: &Value) -> Value {
-    match items {
-        Value::Array(arr) => Value::Array(arr.iter().flat_map(|v| function(v)).collect()),
-        _ => Value::Array(vec![]),
-    }
-}
-
-fn map(function: impl Fn(&Value) -> Value, items: &Value) -> Value {
-    match items {
-        Value::Array(arr) => Value::Array(arr.iter().map(|item| function(item)).collect()),
-        _ => Value::Null,
-    }
-}
-
-fn path<'a>(arg: Value, value: &'a Value) -> &'a Value {
-    fn eval_path<'a>(arg: &[&str], value: &'a Value) -> &'a Value {
-        if arg.is_empty() {
-            return value;
-        }
-        let (first, rest) = arg.split_first().unwrap();
-        if let Value::Object(map) = value {
-            if let Some(v) = map.get(*first) {
-                return eval_path(rest, v);
-            }
-        }
-        &Value::Null
-    }
-
-    match arg {
-        Value::String(s) => eval_path(&[&s], value),
-        Value::Array(arr) => {
-            let paths: Vec<&str> = arr
-                .iter()
-                .filter_map(|v| {
-                    if let Value::String(s) = v {
-                        Some(s.as_str())
-                    } else {
-                        None
-                    }
-                })
-                .collect();
-            eval_path(&paths, value)
-        }
-        _ => &Value::Null,
-    }
 }
 
 #[cfg(test)]
