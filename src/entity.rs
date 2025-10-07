c1ddf1382173ccf3938ff161efd8ba40ae22ab39
Baard H. Rehn Johansen <baard@rehn.no>
2025-10-07 17:01:44 +0200

    got ser/deser to work with uri

--- src/entity.rs
+++ src/entity.rs
@@ -1,9 +1,10 @@
+use core::fmt;
 use std::collections::HashMap;
 
-use serde::{Deserialize, Serialize};
+use serde::{de::{self, DeserializeSeed, MapAccess, SeqAccess, Visitor}, Deserialize, Serialize};
 use serde_json::Number;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 struct Entity {
     #[serde(rename = "_id")]
     id: String,
@@ -13,18 +14,20 @@ struct Entity {
     content: HashMap<String, EntityValue>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Debug, PartialEq)]
 struct Uri {
     uri: String,
 }
 
 impl Uri {
     fn new(arg: &str) -> Self {
-        Self { uri: arg.to_owned() }
+        Self {
+            uri: arg.to_owned(),
+        }
     }
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Debug, PartialEq)]
 enum EntityValue {
     Null,
     Bool(bool),
@@ -38,7 +41,8 @@ enum EntityValue {
 impl Serialize for Uri {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
-        S: serde::Serializer {
+        S: serde::Serializer,
+    {
         serializer.serialize_str(&format!("~u{}", self.uri))
     }
 }
@@ -60,6 +64,181 @@ impl Serialize for EntityValue {
     }
 }
 
+struct KeyClassifier;
+
+enum KeyClass {
+    Map(String),
+}
+
+impl<'de> DeserializeSeed<'de> for KeyClassifier {
+    type Value = KeyClass;
+
+    fn deserialize<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        deserializer.deserialize_str(self)
+    }
+}
+
+impl<'de> Visitor<'de> for KeyClassifier {
+    type Value = KeyClass;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("a string key")
+    }
+
+    fn visit_str<E>(self, s: &str) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        match s {
+            _ => Ok(KeyClass::Map(s.to_owned())),
+        }
+    }
+
+    fn visit_string<E>(self, s: String) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        match s.as_str() {
+            _ => Ok(KeyClass::Map(s)),
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for EntityValue {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        struct EntityValueVisitor;
+
+        impl<'de> Visitor<'de> for EntityValueVisitor {
+            type Value = EntityValue;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                formatter.write_str("any valid JSON value")
+            }
+
+            #[inline]
+            fn visit_bool<E>(self, value: bool) -> Result<EntityValue, E> {
+                Ok(EntityValue::Bool(value))
+            }
+            #[inline]
+
+            fn visit_i64<E>(self, value: i64) -> Result<EntityValue, E> {
+                Ok(EntityValue::Number(value.into()))
+            }
+
+            fn visit_i128<E>(self, value: i128) -> Result<EntityValue, E>
+            where
+                E: serde::de::Error,
+            {
+                let de = serde::de::value::I128Deserializer::new(value);
+                Number::deserialize(de).map(EntityValue::Number)
+            }
+
+            #[inline]
+            fn visit_u64<E>(self, value: u64) -> Result<EntityValue, E> {
+                Ok(EntityValue::Number(value.into()))
+            }
+
+            fn visit_u128<E>(self, value: u128) -> Result<EntityValue, E>
+            where
+                E: serde::de::Error,
+            {
+                let de = serde::de::value::U128Deserializer::new(value);
+                Number::deserialize(de).map(EntityValue::Number)
+            }
+
+            #[inline]
+            fn visit_f64<E>(self, value: f64) -> Result<EntityValue, E> {
+                Ok(Number::from_f64(value).map_or(EntityValue::Null, EntityValue::Number))
+            }
+
+            #[inline]
+            fn visit_str<E>(self, value: &str) -> Result<EntityValue, E>
+            where
+                E: serde::de::Error,
+            {
+                self.visit_string(String::from(value))
+            }
+
+            #[inline]
+            fn visit_string<E>(self, value: String) -> Result<EntityValue, E> {
+                if value.starts_with("~u") {
+                    Ok(EntityValue::Uri(Uri{uri: value[2..].to_owned()}))
+                } else {
+                    Ok(EntityValue::String(value))
+                }
+            }
+
+            #[inline]
+            fn visit_none<E>(self) -> Result<EntityValue, E> {
+                Ok(EntityValue::Null)
+            }
+
+            #[inline]
+            fn visit_some<D>(self, deserializer: D) -> Result<EntityValue, D::Error>
+            where
+                D: serde::Deserializer<'de>,
+            {
+                Deserialize::deserialize(deserializer)
+            }
+
+            #[inline]
+            fn visit_unit<E>(self) -> Result<EntityValue, E> {
+                Ok(EntityValue::Null)
+            }
+
+            #[inline]
+            fn visit_seq<V>(self, mut visitor: V) -> Result<EntityValue, V::Error>
+            where
+                V: SeqAccess<'de>,
+            {
+                let mut vec = Vec::new();
+
+                while let Some(elem) = (visitor.next_element())? {
+                    vec.push(elem);
+                }
+
+                Ok(EntityValue::Array(vec))
+            }
+
+            fn visit_map<V>(self, mut visitor: V) -> Result<EntityValue, V::Error>
+            where
+                V: MapAccess<'de>,
+            {
+                match (visitor.next_key_seed(KeyClassifier))? {
+                    #[cfg(feature = "arbitrary_precision")]
+                    Some(KeyClass::Number) => {
+                        let number: NumberFromString = tri!(visitor.next_value());
+                        Ok(Value::Number(number.value))
+                    }
+                    #[cfg(feature = "raw_value")]
+                    Some(KeyClass::RawValue) => {
+                        let value = tri!(visitor.next_value_seed(crate::raw::BoxedFromString));
+                        crate::from_str(value.get()).map_err(de::Error::custom)
+                    }
+                    Some(KeyClass::Map(first_key)) => {
+                        let mut values = HashMap::new();
+
+                        values.insert(first_key, (visitor.next_value())?);
+                        while let Some((key, value)) = (visitor.next_entry())? {
+                            values.insert(key, value);
+                        }
+
+                        Ok(EntityValue::Object(values))
+                    }
+                    None => Ok(EntityValue::Object(HashMap::new())),
+                }
+            }
+        }
+        deserializer.deserialize_any(EntityValueVisitor)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -72,10 +251,7 @@ mod tests {
             id: "1".to_owned(),
             filtered: false,
             content: HashMap::from([
-                (
-                    "string".to_owned(),
-                    EntityValue::String("value".to_owned()),
-                ),
+                ("string".to_owned(), EntityValue::String("value".to_owned())),
                 ("uri".to_owned(), EntityValue::Uri(Uri::new("db.no"))),
                 (
                     "num".to_owned(),
@@ -109,5 +285,6 @@ mod tests {
 
         // Prints deserialized = Entity { .. }
         println!("deserialized = {:?}", deserialized);
+        assert_eq!(entity, deserialized);
     }
 }
